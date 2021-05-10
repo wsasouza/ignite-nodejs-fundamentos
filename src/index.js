33a44e000328c709b4040f9ef1d73854ea5c1977
wsasouza <wsasouza@hotmail.com>
2021-05-09 22:59:38 -0300

    validating existing CPF

--- src/index.js
+++ src/index.js
@@ -8,17 +8,23 @@ app.use(express.json());
 const customers = [];
 
 app.post("/account", (request, response) => {
-  const { cpf, name } = request.body;  
+  const { cpf, name } = request.body;
 
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }  
 
   customers.push({
     cpf,
     name,
-    id,
-    statement: []
+    id: uuidv4(),
+    statement: [],
   });
-  
+
   return response.status(201).send();
 });
 
